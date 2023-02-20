57cdc3b8695b03acf133ed3bd3c89d66c1b28c6a
Romango <homer37@yandex.ru>
2023-02-20 09:13:34 +0400

    редактирвоал стили

--- src/App.tsx
+++ src/App.tsx
@@ -31,30 +31,6 @@ function App() {
       <Table />
       {error && <h2>you got and error: {error}</h2>}
       {status === "pending" && <div>data is loading</div>}
-      <div>{line.length}</div>
-      <div>{entity}</div>
-      <button
-        onClick={() =>
-          dispatch(
-            createLine({
-              child: [null],
-              equipmentCosts: 0,
-              estimatedProfit: 0,
-              machineOperatorSalary: 0,
-              mainCosts: 0,
-              materials: 0,
-              mimExploitation: 0,
-              overheads: 0,
-              parentId: null,
-              rowName: "",
-              salary: 0,
-              supportCosts: 0,
-            })
-          )
-        }
-      >
-        create new line
-      </button>
     </>
   );
 }
